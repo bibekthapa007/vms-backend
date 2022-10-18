1009cf422dba5d3bcde52eb4c0009a28bd1d366f
bibek_thapa <bibekthapa922@gmail.com>
2022-10-18 16:10:02 +0545

    add morgan

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import "dotenv/config";
 import express from "express";
 import http from "http";
+import morgan from "morgan";
+
 
 const app = express();
 const server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => res.status(200).send("<h1>Vaccine Management App Api</h1>"));
 
