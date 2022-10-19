ae4673e429c9e5f243560ffa7019d04cd1d992bd
bibek_thapa <bibekthapa922@gmail.com>
2022-10-19 17:58:28 +0545

    add auth rate limiter

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,7 +1,19 @@
 import { Router } from 'express';
+import rateLimit from 'express-rate-limit';
 import authController from '../controllers/authController';
 
 const router = Router();
+
+const authLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 20,
+  message: {
+    error: true,
+    message: 'Too many requests! Please try again after 1 hrs.',
+  },
+});
+
+router.use(authLimiter);
 
 router.route('/signup').post(authController.signup);
 router.route('/signin').post(authController.signin);
